0fd1bd39abf99b0a1a685a19db8a526f75b524ee
abidkhan484 <abidkhan484@gmail.com>
2020-03-18 18:04:18 +0600

    second task completed of day 1

--- python3/adventOfCode_day1.py
+++ python3/adventOfCode_day1.py
@@ -3,11 +3,19 @@
 
 file_ = "aoc_d1_input.txt"
 
+def requirement(i, res=0):
+    t = ((i // 3)-2)
+    if (t <= 0):
+        return res
+    res += t
+    return requirement(t, res)
+
+
 res = 0
 with open(file_) as f:
     data = map(int, f.readlines())
     for i in data:
-        res += ((i // 3)-2)
+        res += requirement(i)
 
 print(res)
 
